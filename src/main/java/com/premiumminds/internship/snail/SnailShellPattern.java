f5c13839586eef38d68adec93cacf9a87e9734e5
marianamr17 <136084381+marianamr17@users.noreply.github.com>
2023-06-10 12:07:01 +0100

    Update SnailShellPattern.java

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -15,14 +15,31 @@ class SnailShellPattern implements ISnailShellPattern {
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
    // throw new RuntimeException("Not Implemented Yet");
-    		int [] v = new int [(matrix.length * matrix[0].length)]; // vetor com o número de entradas da matriz
+    		CompletableFuture<int[]> future = new CompletableFuture<>();
+		int[] v = new int[(matrix.length * matrix[0].length)]; // vetor com o número de entradas da matriz
 		int iv = 0; // iterador do vetor
 		int baixo = matrix.length - 1;
 		int direita = (matrix[0].length - 1);
 		int cima = 0;
 		int esquerda = 0;
+		
+		
+		if( matrix == null || matrix.length == 0) {
+			int[] v0 = new int [0];
+			future.complete(v0);
+			return future;
+		}
 
 		while (true) {
+			// correr matriz da esquerda para a direita
+			for (int i = esquerda; i <= direita; i++) {
+				v[iv] = (matrix[cima][i]);
+				iv++;
+			}
+			cima++;
+
+			if (esquerda > direita || cima > baixo)
+				break;
 
 			// correr matriz de cima para baixo
 			for (int i = cima; i <= baixo; i++) {
@@ -30,38 +47,33 @@ class SnailShellPattern implements ISnailShellPattern {
 				iv++;
 			}
 			direita--;
-			if (esquerda > direita || cima > baixo)
-				break;
 
-			// correr matriz da esquerda para a direita
-			for (int i = esquerda; i <= direita; i++) {
-				v[iv] = (matrix[cima][i]);
-				iv++;
-			}
-			cima++;
 			if (esquerda > direita || cima > baixo)
 				break;
 
 			// correr matriz de baixo para cima
+			for (int i = direita; i >= esquerda; i--) {
+				v[iv] = (matrix[baixo][i]);
+				iv++;
+			}
+			baixo--;
+
+			if (esquerda > direita || cima > baixo)
+				break;
+
+			// correr matriz da direita para a esquerda
 			for (int i = baixo; i >= cima; i--) {
 				v[iv] = (matrix[i][esquerda]);
 				iv++;
 			}
 			esquerda++;
-			if ( esquerda > direita || cima > baixo)
-				break;
 
-			// correr matriz da direita para a esquerda
-			for (int i = direita; i >= esquerda; i--) {
-				v[iv] = (matrix[i][esquerda]);
-				iv++;
-			}
-			baixo--;
 			if (esquerda > direita || cima > baixo)
 				break;
 
 		}
-		return   v;
+		future.complete(v);
+		return future;
 	}
-  }
+
 }
