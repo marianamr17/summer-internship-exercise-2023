53ac6ed9bf30cce56c58c25fadc033fb7d0fda86
marianamr17 <136084381+marianamr17@users.noreply.github.com>
2023-06-10 12:08:47 +0100

    Update SnailShellPatternTest.java

--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -22,7 +22,14 @@ public class SnailShellPatternTest {
    * If you want, you can make others :)
    *
    */
-  public SnailShellPatternTest() {
+  //public SnailShellPatternTest() {
+  public Future<int[]> SnailShellPatternTest() {
+	  int[][] matrix = { { 1, 2, 3, 4, 5, 6, 7 }, { 8, 9, 10, 11, 12, 13, 14 }, { 15, 16, 17, 18, 19, 20, 21 },
+				{ 22, 23, 24, 25, 26, 27, 28 }, { 29, 30, 31, 32, 33, 34, 35 }, { 36, 37, 38, 39, 40, 41, 42 },
+				{ 43, 44, 45, 46, 47, 48, 49 } };
+		  Future<int[]> v = getSnailShell(matrix);
+		  return v;
+	}
   };
 
   @Test
