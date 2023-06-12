e4c064b20f3dda989d574ef32365756fed1df370
marianamr17 <136084381+marianamr17@users.noreply.github.com>
2023-06-12 13:09:26 +0100

    Update SnailShellPatternTest.java

--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -42,4 +42,21 @@ public class SnailShellPatternTest {
     int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     assertArrayEquals(result, expected);
   }
+	
+@Test
+public void ScreenLockinPatternTestFirst5Length2Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { 
+      { 1, 2, 3, 4, 5 }, 
+      { 16, 17, 18, 19, 6 }, 
+      { 15, 24, 25, 20, 7 }, 
+      { 14, 23, 22, 21, 8 }, 
+      { 13, 12, 11, 10, 9 } 
+    };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+    assertArrayEquals(result, expected);
+}
+
 }
